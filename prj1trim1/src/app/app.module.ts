591558e333b45ace7b66e77583501f227da7780d
profdanilominas <prof.danilo.minas@gmail.com>
2021-08-03 20:42:39 -0300

    Aula dia 03/08

--- prj1trim1/src/app/app.module.ts
+++ prj1trim1/src/app/app.module.ts
@@ -3,26 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { InicialComponent } from './components/inicial/inicial.component';
-import { CadastroComponent } from './components/cadastro/cadastro.component';
-import { OpcoesComponent } from './components/opcoes/opcoes.component';
-import { ContaComponent } from './components/conta/conta.component';
-import { InicialModule } from './components/inicial/inicial.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    InicialComponent,
-    CadastroComponent,
-    OpcoesComponent,
-    ContaComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    InicialModule,
-    BrowserAnimationsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
