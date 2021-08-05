6239c302e19cf588da6030ed6bd50276b7266603
profdanilominas <prof.danilo.minas@gmail.com>
2021-08-04 22:52:40 -0300

    Início da criação do servidor da API

---
+++ api/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const servidor = express()
+
+servidor.get(
+    '/', (req, res, next) => {
+        return res.send(
+            {
+                "mensagem": "Bem-vindo(a) ao servidor"
+            }
+        )
+    }
+)
+
+servidor.listen(3000, () => {
+    console.log('Servidor funcionando!')
+})
